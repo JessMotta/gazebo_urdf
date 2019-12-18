ce6aa8bb26917f285852aad36251bd6ce717dc40
JessMotta <jessicalimamotta@gmail.com>
2019-12-18 12:46:07 -0300

    add inertial

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,5 +5,6 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("gazebo_sim")
 subdirs("gazebo_simulation")
 subdirs("gazebo_urdf")
