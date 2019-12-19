f92f730e08c1c2cea61d1eed610d26aff4e586d3
JessMotta <jessicalimamotta@gmail.com>
2019-12-19 14:49:29 -0300

    refresh 3.0

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,7 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("gazebo_control")
 subdirs("gazebo_sim")
 subdirs("gazebo_simulation")
 subdirs("gazebo_urdf")
